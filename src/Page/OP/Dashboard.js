9e51458c2293202e9692fd9014de6e51ac021c26
Atchraphon <atchraphon@playtorium.co.th>
2021-05-09 19:00:34 +0700

    add OP folder

---
+++ src/Page/OP/Dashboard.js
@@ -0,0 +1,122 @@
+import React from "react";
+import {
+    Row, Col, Card
+} from 'antd';
+import './Dashboard.css'
+import { Chart } from "react-charts";
+import CardTable from '../../Component/CardTable'
+
+const Dashboard = () => {
+    //   useLagRadar();
+
+    const data = React.useMemo(
+        () => [
+            {
+                label: 'Series 1',
+                data: [
+                    { primary: 1, secondary: 10 },
+                    { primary: 2, secondary: 15 },
+                    { primary: 3, secondary: 7 },
+                ],
+            },
+            {
+                label: 'Series 2',
+                data: [
+                    { primary: 1, secondary: 9 },
+                    { primary: 2, secondary: 15 },
+                    { primary: 3, secondary: 20 },
+                ],
+            },
+            {
+                label: 'Series 3',
+                data: [
+                    { primary: 1, secondary: 10 },
+                    { primary: 2, secondary: 15 },
+                    { primary: 3, secondary: 18 },
+                ],
+            },
+        ],
+        []
+    )
+
+    const axes = React.useMemo(
+        () => [
+            { primary: true, type: 'ordinal', position: 'bottom' },
+            { type: 'linear', position: 'left' },
+        ],
+        []
+    )
+
+    const series = React.useMemo(
+        () => ({
+            type: "bar"
+        }),
+        []
+    );
+
+    const cilentData = [
+        {
+            no: 1,
+            client: 'AA',
+            phone: '098765555',
+            email: 'acc@gmail.com'
+        },
+        {
+            no: 2,
+            client: 'AA',
+            phone: '098765555',
+            email: 'acc@gmail.com'
+        },
+        {
+            no: 3,
+            client: 'AA',
+            phone: '098765555',
+            email: 'acc@gmail.com'
+        },
+        {
+            no: 4,
+            client: 'AA',
+            phone: '098765555',
+            email: 'acc@gmail.com'
+        },
+        {
+            no: 5,
+            client: 'AA',
+            phone: '098765555',
+            email: 'acc@gmail.com'
+        },
+    ]
+
+    return (
+        <div className="dashboard-page">
+            <Row justify='space-around' style={{ padding: '40px' }}>
+                <Col>
+                    <Card className="card-container">
+                        <div style={{ marginBottom: '10px' }}>Total Number of Clients Per Month</div>
+                        <div className="graph">
+                            <Chart data={data} axes={axes} series={series} />
+                        </div>
+                    </Card>
+                </Col>
+                <Col className="op-task-table">
+                    <CardTable title='Task' data={cilentData} isDashboard={true} />
+                </Col>
+            </Row>
+            <Row justify='space-around' style={{ padding: '40px' }}>
+                <Col className="op-task-table">
+                    <CardTable title='Clients' data={cilentData} isDashboard={true} />
+                </Col>
+                <Col>
+                    <Card className="card-container">
+                        <div style={{ marginBottom: '10px' }}>Total Number of Clients Per Month</div>
+                        <div className="graph">
+                            <Chart data={data} axes={axes} series={series} />
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+export default Dashboard
